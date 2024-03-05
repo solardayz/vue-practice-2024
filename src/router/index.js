ad5ab4fb9afcf9685a64663272d7ea3b5ae99fc5
jhcha <jhc@gsitm.com>
2024-03-05 11:46:14 +0900

    데이터 바인딩 체크박스

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/databinding/string',
@@ -48,6 +49,14 @@ const routes = [
     component: () =>
       import(
         /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/01_databinding/DataBindingSelectView.vue'
+      )
+  },
+  {
+    path: '/databinding/check',
+    name: 'DataBindingCheckboxView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/01_databinding/DataBindingCheckboxView.vue'
       )
   }
 ]
---
+++ src/views/01_databinding/DataBindingCheckboxView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="checkbox"
+        name=""
+        id="html"
+        value="HTML"
+        v-model="favoriteLang"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        name=""
+        id="js"
+        value="JS"
+        v-model="favoriteLang"
+      />
+      <label for="js">JS</label>
+      <input
+        type="checkbox"
+        name=""
+        id="css"
+        value="CSS"
+        v-model="favoriteLang"
+      />
+      <label for="css">CSS</label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      favoriteLang: ['CSS']
+    }
+  }
+}
+</script>
