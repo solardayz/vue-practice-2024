115d90fc0b9798002ae10aaba999f0de1cc0c4cb
jhcha <solardayz@naver.com>
2024-03-09 13:27:20 +0900

    나브, 푸터

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 Vue.config.productionTip = false
 
