4bbb4206d49503e45dac9c43ecca3cc5cc7096de
jhcha <solardayz@naver.com>
2024-03-09 14:16:39 +0900

    부트스트랩 부트스트랩 아이콘 사용

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 Vue.config.productionTip = false
 
